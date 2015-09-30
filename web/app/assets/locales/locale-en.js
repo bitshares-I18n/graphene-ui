cb9b2df08279ab260398133396b7ea6dab2fd7e0
rng-lab <rnglab@gmail.com>
2015-09-30 00:15:22 -0300

    Update locale-en.js

--- web/app/assets/locales/locale-en.js
+++ web/app/assets/locales/locale-en.js
@@ -4,7 +4,8 @@ module.exports = {
         cn: "简体中文",
         fr: "Français",
         ko: "한국어",
-        de: "Deutsch"
+        de: "Deutsch",
+        es: "Spanish"
     },
     header: {
         title: "Graphene UI",
